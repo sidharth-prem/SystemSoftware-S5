02a4d0bae142c78cc3b21f5b90146d25c66e3993
SIDHARTH PREM <95079525+sidharth-prem@users.noreply.github.com>
2023-11-23 17:10:32 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,70 @@
+#include<stdio.h> 
+#include<string.h>
+#include<ctype.h>
+void main()
+{
+ FILE *fint,*fcode,*flen,*fsym;
+ int opl[10],txtlen,txtlen1,i,j=0,len;
+ char
+ add[5],symadd[5],op[5],start[10],temp[30],line[20],label[20],mne[10],operand[10],symtab[10],opmne[10];
+ fint=fopen("intermediate.txt","r");
+ flen=fopen("length.txt","r");
+ fcode=fopen("opcode.txt","r");
+ fsym=fopen("output.txt","r");
+ fscanf(fint,"%s%s%s%s",add,label,mne,operand);
+ if(strcmp(mne,"START")==0)
+ {
+  strcpy(start,operand);
+  fscanf(flen,"%d",&len);
+  }
+printf("H^%s^%s^%d\nT^00%s^",label,start,len,start);
+ fscanf(fint,"%s%s%s%s",add,label,mne,operand);
+ while(strcmp(mne,"END")!=0)
+ {
+ fscanf(fcode,"%s%s",opmne,op);
+ while(!feof(fcode))
+ {
+ if(strcmp(mne,opmne)==0)
+ {
+ fclose(fcode);
+ fscanf(fsym,"%s%s",symadd,symtab);
+ while(!feof(fsym))
+ {
+ if(strcmp(operand,symtab)==0)
+ {
+  printf("%s%s^",op,symadd);
+  break;
+  }  
+else
+fscanf(fsym, "%s %s",symadd,symtab);
+}
+break;
+}
+else
+fscanf(fsym, "%s %s",opmne,op);
+}
+if((strcmp(mne, "BYTE")==0) || (strcmp(mne, "WORD") == 0))
+{
+ if(strcmp(mne,"WORD")==0)
+ 	printf("0000%s^", operand);
+ else
+ {
+  len = strlen(operand);
+  for(i=2;i<len;i++)
+  {
+   printf("%d",operand[i]);
+  }
+  printf("^");
+ }
+}
+fscanf(fint,"%s%s%s%s", add, label, mne, operand);
+fcode = fopen("opcode.txt", "r");
+fseek(fcode, SEEK_SET, 0);
+}
+printf("\nE^00%s",start);
+fclose(fint);
+fclose(fcode);
+fclose(fsym);
+fclose(flen);
+}
+
